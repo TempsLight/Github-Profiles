e3eb324a7ca19be41d7635d6791b41739daf9c71
Temps Light <90301543+TempsLight@users.noreply.github.com>
2023-07-05 10:11:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const APIURL = 'https://api.github.com/users/'
+const form = document.getElementById('form')
+const main = document.getElementById('main');
+const search = document.getElementById('search')
+async function getUser(username){
+    try {
+        const {data} = await axios(APIURL + username)
+    
+    createUserCard(data)
+    }
+    catch(err){
+        console.log(err)
+    }  
+}
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const user = search.value
+
+  if(user) {
+    getUser(user)
+
+    search.value = ''
+  }
+})
+
+
+function createUserCard(user){
+  const cardHtml = `
+  <div class="card">
+        <div>
+             <img src="${user.avatar_url}" alt="" class="avatar">
+        </div>
+        <div class="user-info">
+            <h2>${user.name}</h2>
+            <p> ${user.bio}</p>
+            <ul>
+                <li> ${user.followers} <strong> Followers </strong>
+                </li>
+                <li> ${user.following} <strong> Following </strong>
+                </li>
+                <li> ${user.public_repos} <strong> Repos </strong>
+                </li>
+            </ul>
+            
+        </div>
+       </div>`;
+  main.innerHTML = cardHtml
+}
+
+
+
+
+
+
